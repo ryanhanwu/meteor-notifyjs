131ee127366ac7aaf55e78146ae9cb5dc6b69f19
Ryan Wu <hello@ryanwu.me>
2015-09-19 20:27:24 -0400

    add submodule

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
 	name: 'ryanwu:meteor-notefyjs',
 	version: '0.1.0',
 	// Brief, one-line summary of the package.
-	summary: 'A jQuery plugin to provide simple yet fully customisable notifications.',
+	summary: 'A simple, versatile jQuery notification library',
 	// URL to the Git repository containing the source code for this package.
-	git: 'https://github.com/ryanhanwu/meteor-gridstack.git',
+	git: 'https://github.com/ryanhanwu/meteor-notifyjs.git',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md'
@@ -13,7 +13,5 @@ Package.describe({
 Package.onUse(function(api) {
 	api.versionsFrom('1.1.0.3');
 	api.use('jquery', 'client');
-	api.add_files('lib/gridstack.js/dist/gridstack.min.css', 'client');
-	api.add_files('lib/gridstack.js/dist/gridstack-extra.min.css', 'client');
-	api.add_files('lib/gridstack.js/dist/gridstack.min.js', 'client');
+	api.add_files('lib/notifyjs/notify-combined.min.js', 'client');
 });
